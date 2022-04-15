cd3edde6714dc666f9eabfb9f8655e64edc5bb1d
Hollie Hensman <hensmanh@gmail.com>
2022-04-15 19:12:12 +0000

    Fail validation when creating/saving new store

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -22,3 +22,8 @@ class Store < ActiveRecord::Base
   validates_length_of :name, minimum: 3
   validates_numericality_of :annual_revenue, greater_than_or_equal_to: 0
 end
+
+store7 = Store.new
+store7.save
+store7.save!
+# raise_record_invalid': Validation failed: Name is too short (minimum is 3 characters), Annual revenue is not a number (ActiveRecord::RecordInvalid)
